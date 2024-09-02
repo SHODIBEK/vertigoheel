dc4541a769e54ee3b60021493725bee8cfa714e1
Shodibek Musaev <shodik91@gmail.com>
2024-09-03 00:10:54 +0500

    add modal

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,18 @@ import { ieFix } from "./vendor/ie-fix";
 import { vhFix } from "./vendor/vh-fix";
 import { actualYear } from "./modules/actualYear";
 import { headerFixed } from "./components/headerFixed";
+import MicroModal from "micromodal";
 
 ieFix();
 vhFix();
 actualYear();
 header.init();
 headerFixed();
+MicroModal.init({
+	openTrigger: "data-modal-open",
+	closeTrigger: "data-modal-close",
+	openClass: "is-open",
+	awaitOpenAnimation: true,
+	awaitCloseAnimation: true,
+	disableScroll: true,
+});
